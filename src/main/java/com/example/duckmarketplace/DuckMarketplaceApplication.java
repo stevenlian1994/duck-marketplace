8fa6075e5d28de7209d28907e0490a1f8c022163
Steven <stevenlian1994@gmail.com>
2021-07-05 19:18:44 -0700

    gradle springboot addition

---
+++ src/main/java/com/example/duckmarketplace/DuckMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.duckmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/duckmarketplace/DuckMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duckmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
